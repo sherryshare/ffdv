2deb07b1fbb6a191dd7ac59f93bd26c14013ffd6
Athrun Arthur <athrunarthur@gmail.com>
2013-02-18 13:21:06 +0800

    clean build directory

---
+++ manager/build/CMakeFiles/client_deamon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/client_deamon.dir/client.cpp.o"
+  "client_deamon.pdb"
+  "client_deamon"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/client_deamon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ manager/build/CMakeFiles/master.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/athrun/projects_git/ffdv/manager/master.cpp" "/home/athrun/projects_git/ffdv/manager/build/CMakeFiles/master.dir/master.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PROTO_BUF_SUPPORT"
+  "ENABLE_LOG_CONSOLE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  ".."
+  "/home/athrun/boost_1_52_0"
+  "/home/sherry/ffnet-master/network"
+  "/home/sherry/ffdv/net"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
